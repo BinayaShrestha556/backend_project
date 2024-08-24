1e32503800c24506f2561b126b19e57e758c3ebe
BinayaShrestha556 <binayastha37b@gmail.com>
2024-08-24 15:01:38 +0545

    added some controllers

--- src/controllers/subscriber.controller.js
+++ src/controllers/subscriber.controller.js
@@ -25,5 +25,47 @@ const subscribeTo=asyncHandler(async(req,res,next)=>{
     return res.status(200).json(new ApiRes(200,{},"subscribed"))
 
 })
+const getSubscriptions=asyncHandler(async(req,res)=>{
+    const {_id}=req.user
+    if(!_id){
+        throw new ApiError(400,"user id not found");
 
-export {subscribeTo}
+    }
+    const subscriptions= await Subscription.aggregate([
+        {
+            $match:{
+                subscriber:_id
+            }
+        },
+        {
+         $lookup:{
+            from:"users",
+            foreignField:"_id",
+            localField:"channel",
+            as:"channel",
+            pipeline:[{
+                
+                $project:{
+                    username:1,
+                    avatar:1
+                }
+            }
+                
+            ]
+         }   
+        },
+      {
+        $unwind:"$channel"
+      },
+      {
+        $project:{
+            subscriber:0
+        }
+      }
+    ])
+    
+    return res.status(200).json(new ApiRes(200,subscriptions,"success"))
+    
+})
+
+export {subscribeTo,getSubscriptions}
--- src/routes/subscription.router.js
+++ src/routes/subscription.router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { subscribeTo } from "../controllers/subscriber.controller.js";
+import { getSubscriptions, subscribeTo } from "../controllers/subscriber.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router=Router()
 router.post("/subscribeTo",verifyJWT,subscribeTo)
+router.post("/subscriptions",verifyJWT,getSubscriptions)
+
 
 export default router
